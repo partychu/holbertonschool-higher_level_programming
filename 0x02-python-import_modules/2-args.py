e3d52aa8cc2ff1b351b6a786a109bac4eac30362
yatoro667 <ytorres667@gmail.com>
2021-04-29 22:42:26 +0000

    Incomplete

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    argc = len(argv) - 1
+
+    if argc = 0:
+        print("0 arguments.")
+    elif argc = 1:
+        print("1 argument:")
+    elif argc > 1:
+        print("{} arguments:".format(argc))
+        for
