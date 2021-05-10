9bdb7903071fd46ee0aad8f423f206fa8fd9ba55
yatoro667 <ytorres667@gmail.com>
2021-05-10 21:08:22 +0000

    Checking 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    argv = sys.argv
-    argc = len(argv) - 1
+    from sys import argv
 
-    if argc = 0:
+    argc = len(argv)
+
+    if argc is 1:
         print("0 arguments.")
-    elif argc = 1:
+    if argc is 2:
         print("1 argument:")
-    elif argc > 1:
-        print("{} arguments:".format(argc))
-        for i in range(1, argc):
-            print("{}: {}".format(i, argv[i]))
+    if argc > 2:
+        print("{:d} arguements:".format(argc - 1))
+    for i in range(1, argc):
+        print("{:d}: {:s}".format(i, argv[i]))
