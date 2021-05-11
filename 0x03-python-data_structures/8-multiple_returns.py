72e7bec3b5acbb36966a253ccc010f3b63e180d7
yatoro667 <ytorres667@gmail.com>
2021-05-11 03:30:42 +0000

    Checking 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is 0:
-        sentence[0] = None
-        return len(sentence), sentence[0]
+    if len(sentence) is 0:
+        return len(sentence), None
     return len(sentence), sentence[0]
