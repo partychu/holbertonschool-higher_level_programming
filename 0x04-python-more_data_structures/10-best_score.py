6672b1e7dd6414698602e57a431b369307f4e670
yatoro667 <ytorres667@gmail.com>
2021-07-01 23:18:29 +0000

    Checking 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     score = 0
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     for k, v in a_dictionary.items():
         if v > score:
