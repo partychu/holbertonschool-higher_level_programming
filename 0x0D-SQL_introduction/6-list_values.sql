698c873cc925c775c6ba0458f4cc2d2eca52eaef
yatoro667 <ytorres667@gmail.com>
2021-07-13 23:38:07 +0000

    Checking 6-9

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- lists all rows of table first_table
+-- prints all fields
+SELECT * FROM first_table;
---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- create table second_table 
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	SCORE INT
+	);
+	INSERT INTO second_table (id, name, score)
+	VALUES
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
+
