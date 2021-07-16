a8c2f8e7096253745a4f242d535edf05bf0eea16
yatoro667 <ytorres667@gmail.com>
2021-07-16 05:51:16 +0000

    Checking 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- creates MYSQL server user uder_0d_1
+-- all privileges, pw set, checks if already exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
