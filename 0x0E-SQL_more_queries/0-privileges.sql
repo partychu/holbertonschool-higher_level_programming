b3bd976116ae581298a2c8a73fe9d9650640d199
yatoro667 <ytorres667@gmail.com>
2021-07-16 05:27:23 +0000

    First commit, checking 0

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- lists all privileges of the MySQL users user 1/2 on my server
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+Project 0x0E SQL More Queries Holberton School SJU Cohort 14
