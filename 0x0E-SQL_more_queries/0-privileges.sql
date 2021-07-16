dddffc3ddd8d28d77932d8a22d8e20e2ae77add1
yatoro667 <ytorres667@gmail.com>
2021-07-16 05:38:50 +0000

    Add comment line, output is correct

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- lists all privileges of the MySQL users user 1/2 on my server
+-- lists all privileges of the MySQL users user 1/2
+-- on local server
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
