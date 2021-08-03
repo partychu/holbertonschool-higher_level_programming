91cf180d0b392b75aa1e455b42e43de58b8596b1
yatoro667 <ytorres667@gmail.com>
2021-08-03 03:48:00 +0000

    adding files 0 - 6

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const p = parseInt(args[0]);
+
+if (isNaN(p)){
+	console.log("Not a number");
+} else {
+	console.log("My number: " + p);
+}
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const a = ["C is fun", "Python is cool", "JavaScript is amazing"]
+
+const l = a.length;
+
+for (let i = 0; i < l; i++) {
+	console.log(a[i]);
+}
