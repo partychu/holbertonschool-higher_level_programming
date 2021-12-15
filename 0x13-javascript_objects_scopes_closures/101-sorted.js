61c6e0168d9d323856c82f157e0d9fa1f4c6aa78
partychu <ytorres667@gmail.com>
2021-12-15 19:35:12 +0000

    Fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,8 +3,10 @@
 const dict = require('./101-data').dict;
 const newdict = {};
 for (const key in dict) {
-  if (newdict[dict[key]] === undefined) { // 1, 2, 1, 3
+  if (newdict[dict[key]] === undefined) {
     newdict[dict[key]] = [];
   }
-  newdict[dict[key]].push(key); // 89, 90, 91,
+  newdict[dict[key]].push(key);
 }
+
+console.log(newdict);
