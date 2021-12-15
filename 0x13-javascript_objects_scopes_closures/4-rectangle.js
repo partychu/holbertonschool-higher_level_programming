9f3f505f4fa5cdb556fa3d869861be6b37cfef81
partychu <ytorres667@gmail.com>
2021-12-15 04:00:19 +0000

    Semistandard fix

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -3,24 +3,24 @@
 class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
-	  this.width = w;
-	  this.height = h;
-	}
+      this.width = w;
+      this.height = h;
+    }
   }
 
   print () {
-	for (let i = 0; i < this.height; i++) {
-	  console.log('X'.repeat(this.width));
-	  }
-	}
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
 
   rotate () {
-	[this.width, this.height] = [this.height, this.width];
-	}
+    [this.width, this.height] = [this.height, this.width];
+  }
 
   double () {
-  this.width = this.width * 2;
-  this.height = this.height * 2;
+    this.width = this.width * 2;
+    this.height = this.height * 2;
   }
 }
 
