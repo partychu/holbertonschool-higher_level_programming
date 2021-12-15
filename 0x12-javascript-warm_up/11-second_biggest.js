2335d250df47d0e03618e399360091849e0003c6
partychu <ytorres667@gmail.com>
2021-12-15 04:12:24 +0000

    Semistandard fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (process.argv.length <= 3) {
-	console.log(0);
+  console.log(0);
 } else {
-	console.log(process.argv.sort().reverse()[1]);
+  console.log(process.argv.sort().reverse()[1]);
 }
