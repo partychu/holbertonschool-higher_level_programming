d12c72d171874bad353f6e10b6db0e66d9f72abf
partychu <ytorres667@gmail.com>
2021-12-15 09:48:42 +0000

    Checking 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const Square5 = require('./5-square');
+
+class Square extends Square5 {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
