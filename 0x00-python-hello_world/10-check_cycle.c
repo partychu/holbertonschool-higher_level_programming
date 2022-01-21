90b23b8eb362d7f1421e93e720e45e76ed3a53bd
partychu <ytorres667@gmail.com>
2022-01-21 15:32:10 +0000

    Mock interview points

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *p1 = list;
+	listint_t *p2 = list;
+
+	while (p2->next != NULL && p2->next->next != NULL)
+	{
+		p1 = p1->next;
+		p2 = p2->next->next;
+		if (p1 == p2)
+			return (1);
+	}
+	return (0);
+}
