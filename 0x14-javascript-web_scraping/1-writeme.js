6a15f4f717ee4ad54166cd3973c01ac97bd6e0a5
partychu <ytorres667@gmail.com>
2022-02-01 20:32:46 +0000

    Checking 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,6 +2,6 @@
 const myArgs = process.argv.slice(2);
 const fs = require('fs');
 
-fs.writeFile(myArgs[0], myArgs[1] + '\n', 'utf8', function (err) {
+fs.writeFile(myArgs[0], myArgs[1], 'utf8', function (err) {
   if (err) return console.log(err);
 });
