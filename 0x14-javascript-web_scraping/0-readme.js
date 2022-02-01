7a1bc6000464f69f254b08c31ac2fb03a5a7a498
partychu <ytorres667@gmail.com>
2022-02-01 12:20:35 +0000

    Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
+const myArgs = process.argv.slice(2)
 fs = require('fs');
 
-fs.readFile('cisfun', 'utf8', (err,data) => {
-	if (err) throw err;
+fs.readFile(myArgs[0], 'utf8', function (err,data) {
+	if (err) {
+	return console.log(err);
+	}
 	console.log(data);
 });
