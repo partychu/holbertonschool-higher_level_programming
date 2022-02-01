45701e729d5779abeba47f53d17aa86cc9381f58
partychu <ytorres667@gmail.com>
2022-02-01 12:38:21 +0000

    Task 0 & 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const myArgs = process.argv.slice(2)
-fs = require('fs');
+const myArgs = process.argv.slice(2);
+const fs = require('fs');
 
-fs.readFile(myArgs[0], 'utf8', function (err,data) {
-	if (err) {
-	return console.log(err);
-	}
-	console.log(data);
+fs.readFile(myArgs[0], 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
 });
---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myArgs = process.argv.slice(2);
+const fs = require('fs');
+
+fs.writeFile(myArgs[0], myArgs[1] + '\n', 'utf8', function (err) {
+  if (err) return console.log(err);
+});
