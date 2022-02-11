72941861e26fe727c091c48dc8b217a9389cabdb
Partychu <ytorres667@gmail.com>
2022-02-11 08:30:23 -0400

    Final semistandard fix

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $('#add_item').click(function() {
+$(document).ready(function () {
+  $('#add_item').click(function () {
     $('.my_list').append('<li>Item</li>');
-    });
+  });
 });
